74eaf6d5e10cf35720a30d55620c0b0a75ff59d6
Maksim Makushchenko <maksalmak@gmail.com>
2020-09-16 16:16:58 +0300

    Init project wwith plugin

---
+++ Assets/Scripts/ControlFutuRift.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ChairControl.ChairWork;
+using ChairControl.ChairWork.Options;
+
+public class ControlFutuRift : MonoBehaviour
+{
+    public UdpOptions udpOptions;
+
+    private FutuRiftController futuRiftController;
+    void OnEnable()
+    {
+        futuRiftController = new FutuRiftController(udpOptions);
+        futuRiftController.Start();
+    }
+
+    void OnDisable()
+    {
+        futuRiftController.Stop();
+    }
+
+    void Update()
+    {
+        
+    }
+}
