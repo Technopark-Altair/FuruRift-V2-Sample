2891fa3b85ed0ba4e39c809375497ffbd5441546
Maksim Makushchenko <maksalmak@gmail.com>
2020-10-08 15:04:32 +0300

    Add VR to sample

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float xtime = 10;
+
+    void Update()
+    {        
+        var xDelta = 2 * Mathf.PI / xtime;
+        var xtimePosition = (Time.time % xtime);
+        var xposition = Mathf.Cos(xtimePosition * xDelta);
+        var xRotation = xposition * 30;
+
+        transform.eulerAngles = new Vector3(xRotation, 0, 0);
+    }
+}
